2add48cec29acd385706827bfae9be02c45a0252
Yolea Mayers <yoleamayers@Yoleas-Air.fios-router.home>
2020-05-13 16:35:16 -0400

    create post is now functional

--- vitae/config/routes.rb
+++ vitae/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  post '/auth/login', to: 'authentication#login'
-  get '/auth/verify', to: 'authentication#verify'
-
+ 
   resources :users do
     resources :posts
     resources :comments
+
+
+    post '/auth/login', to: 'authentication#login'
+    get '/auth/verify', to: 'authentication#verify'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
